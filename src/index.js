4fff40c2c3fe5d77f3649f7a6aac28e12ce1b993
Chowdhurian <manil.chowdhury@gmail.com>
2018-03-31 19:11:04 -0700

    use todomvc css to get started

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { render } from 'react-dom'
 import ListPicker from './components/ListPicker'
+// import '../node_modules/todomvc-common/base.css'
+import '../node_modules/todomvc-app-css/index.css'
 // import './index.css'
 // import registerServiceWorker from './registerServiceWorker';
 
