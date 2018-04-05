6fa8a3edf1726f3254cdc25bd0b34e925ace579f
Chowdhurian <manil.chowdhury@gmail.com>
2018-04-05 15:10:13 -0700

    use and validate props

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import AddItemForm from './AddItemForm'
 
-const Footer = () => {
+const Footer = (props) => {
   return (
-    <div className="footer">footer!</div>
+    <React.Fragment>
+      <div className='footer'>footer!</div>
+      <AddItemForm addItem={props.addItem} />
+      <button onClick={props.loadCoreItems}>load core items</button>
+    </React.Fragment>
   )
+}
+
+Footer.propTypes = {
+  addItem: PropTypes.func.isRequired,
+  loadCoreItems: PropTypes.func.isRequired,
 }
 
 export default Footer
