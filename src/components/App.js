ab48c4725d29ee76679473ba3864457184965178
Chowdhurian <manil.chowdhury@gmail.com>
2018-04-05 15:08:43 -0700

    init state, add class methods for adding items and loading core items

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,38 @@ import React from 'react'
 import Header from './Header'
 import TodoList from './TodoList'
 import Footer from './Footer'
+import coreItems from '../core-items'
 
-const App = () => {
-  return (
-    <div className="press-start-to-begin">
-      <Header subject="computer science" emoji="💻" emojiNoun="laptop" />
-      <TodoList />
-      <Footer />
-    </div>
-  )
+class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      items: [],
+    }
+
+    this.addItem = (item) => {
+      const items = { ...this.state.items }
+
+      items[`fish${Date.now()}`] = item
+
+      this.setState({ items })
+    }
+
+    this.loadCoreItems = () => {
+      this.setState({ items: coreItems })
+    }
+  }
+
+  render() {
+    return (
+      <div className='press-start-to-begin'>
+        <Header subject='computer science' emoji='💻' emojiNoun='laptop' />
+        <TodoList />
+        <Footer addItem={this.addItem} loadCoreItems={this.loadCoreItems} />
+      </div>
+    )
+  }
 }
 
 export default App
