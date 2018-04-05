b82d584efe1d934cc003ad182291c2d4821ddb40
Chowdhurian <manil.chowdhury@gmail.com>
2018-04-04 17:09:58 -0700

    add helpers in own file

---
+++ src/helpers.js
@@ -0,0 +1,45 @@
+function getRandomItem(arr) {
+  return arr[Math.floor(Math.random() * arr.length)]
+}
+
+function getFunName() {
+  const adjectives = [
+    'boundless',
+    'dizzy',
+    'fallacious',
+    'giant',
+    'languid',
+    'mammoth',
+    'spotty',
+    'thoughtful',
+  ]
+
+  const nouns = [
+    'curve',
+    'flesh',
+    'grape',
+    'hammer',
+    'join',
+    'key',
+    'ocean',
+    'representative',
+    'tray',
+    'writer',
+  ]
+
+  const verbs = [
+    'invent',
+    'launch',
+    'preserve',
+    'scare',
+    'turn',
+    'whisper',
+  ]
+
+  return `${getRandomItem(adjectives)}-${getRandomItem(verbs)}-${getRandomItem(nouns)}`
+}
+
+export {
+  getFunName,
+  getRandomItem,
+}
