217b894daf2183c16ec8edb59ac04c5c73342bc3
Chowdhurian <manil.chowdhury@gmail.com>
2018-04-04 17:12:12 -0700

    handle change state event in list picker

--- src/components/ListPicker.js
+++ src/components/ListPicker.js
@@ -1,21 +1,40 @@
 import React from 'react'
+import { getFunName } from '../helpers'
 
-const ListPicker = () => {
-  return (
-    <React.Fragment>
-      <h2>please enter a list</h2>
-      <form className="list-selector">
-        <input type="text" required placeholder="Learning List Subject" />
-        <button type="submit">start</button>
-        <span
-          role="img"
-          aria-label="emoji of clapperboard"
-        >
-          🎬
-        </span>
-      </form>
-    </React.Fragment>
-  )
+class ListPicker extends React.Component {
+  listInput = React.createRef()
+
+  goToList = (event) => {
+    event.preventDefault()
+    // get text from input
+    const listName = this.listInput.current.value
+    // change page to input
+    this.props.history.push(`/list/${listName}`)
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <h2>please enter a list</h2>
+        <form className="list-selector" onSubmit={this.goToList}>
+          <input
+            type="text"
+            ref={this.listInput}
+            required
+            placeholder="Learning List Subject"
+            defaultValue={getFunName()}
+          />
+          <button type="submit">start</button>
+          <span
+            role="img"
+            aria-label="emoji of clapperboard"
+          >
+            🎬
+          </span>
+        </form>
+      </React.Fragment>
+    )
+  }
 }
 
 export default ListPicker
