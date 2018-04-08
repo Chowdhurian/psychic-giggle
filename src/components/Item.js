6cf710a22960a5faf1f3343d76e6f1626fd84e1e
Chowdhurian <manil.chowdhury@gmail.com>
2018-04-07 17:34:52 -0700

    display all item props

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,15 +1,37 @@
 import React from 'react'
 
 const Item = (props) => {
+  const {
+    name,
+    tags,
+    // status,
+    link,
+    load,
+    length,
+    medium,
+  } = props.item
+
   return (
-    <form>
-      <input
-        type='checkbox'
-        id={props.item.name}
-        // checked={props.item.status === 'incomplete'}
-      />
-      <label htmlFor={props.item.name}>{props.item.name}</label>
-    </form>
+    <React.Fragment>
+      <form>
+        <input
+          type='checkbox'
+          id={name}
+          // checked={props.item.status === 'incomplete'}
+        />
+        <label htmlFor={name}>
+          <a href={link}>{name}</a>
+        </label>
+      </form>
+      <div>
+        <span>{load}</span>
+        <span>{medium}</span>
+        <span>{length}</span>
+      </div>
+      <div>
+        {Object.keys(tags).map(key => <span>{tags[key]}</span>)}
+      </div>
+    </React.Fragment>
   )
 }
 
